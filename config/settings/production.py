4f5b4c0db6e0ea39118eb595e4731e864441fd5d
eyob tariku <eyobtariku@gmail.com>
2019-08-28 21:52:33 +0300

    HotFix - Add `gebeyabot.pythonanywhwere.com` to production setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,5 +5,5 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'https://gebeyabot.pythonanywhere.com',
     'http://gebeyabot.pythonanywhere.com',
-    'gebeya.pythonanywhere.com'
+    'gebeyabot.pythonanywhere.com'
 ]
