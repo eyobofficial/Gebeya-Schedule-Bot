800f6b39fd454760fa9cbc782db8c357799b2fb7
eyob tariku <eyobtariku@gmail.com>
2019-08-29 15:22:18 +0300

    Hotfix - Dynamically load setting module in the wsgi.py fiel

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,11 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = [
+    # Pythonanywhere
     'https://gebeyabot.pythonanywhere.com',
     'http://gebeyabot.pythonanywhere.com',
-    'gebeyabot.pythonanywhere.com'
+    'gebeyabot.pythonanywhere.com',
+
+    # DigitalOcean
+    '134.209.244.238',
 ]
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    config("DJANGO_SETTINGS_MODULE")
+)
 
 application = get_wsgi_application()
