b1100532a60c154b049e5c245af496c7e5d71965
eyob tariku <eyobtariku@gmail.com>
2019-08-30 00:16:51 +0300

    [#12] - Generate schedules from ScheduleRule instance

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Track, Course, Schedule
+from .models import Track, Course, Schedule, ScheduleRule, Pattern
 
 
 @admin.register(Track)
@@ -13,6 +13,17 @@ class TrackAdmin(admin.ModelAdmin):
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'code')
     search_fields = ('title', 'code', 'instructor', 'description')
+
+
+class PatternInline(admin.TabularInline):
+    model = Pattern
+
+@admin.register(ScheduleRule)
+class ScheduleRuleAdmin(admin.ModelAdmin):
+    list_display = ('course', 'session', 'start_date', 'end_date')
+    list_filter = ('session', )
+    filter_horizontal = ('tracks', )
+    inlines = (PatternInline, )
 
 
 @admin.register(Schedule)
